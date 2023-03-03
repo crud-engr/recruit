c5a7b7835785af7ce3bd49b1121143fcea7b29ba
Ayinla Abeeb <abeebayinla@gmail.com>
2023-03-03 17:40:28 +0100

    add colo to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       purple: "#CA61C3",
       orange: "#FFCC33",
       grey: "#979797",
+      lightgrey: "rgba(131, 131, 131, 0.08);",
     },
   },
   plugins: [require("daisyui")],
