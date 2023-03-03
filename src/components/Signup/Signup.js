9c2e7d27ce64806634a791d8392d20a3637c7b18
Ayinla Abeeb <abeebayinla@gmail.com>
2023-03-03 16:47:48 +0100

    add signup button

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,13 @@
+import signup from "../../assets/signup-box.svg";
+
+const Signup = () => {
+  return (
+    <div>
+      <div className="w-96 flex justify-center">
+        <img src={signup} alt="signup" />
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
