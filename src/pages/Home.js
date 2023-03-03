bb088b7e812ec57302887fb8f3101a408c900148
Ayinla Abeeb <abeebayinla@gmail.com>
2023-03-03 16:49:45 +0100

    add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,17 @@
 import Animate from "../components/Animate/Animate";
 import Container from "../components/Container/Container";
 import Navigation from "../components/Navigation/Navigation";
+import Search from "../components/Search/Search";
+import MainLayout from "../components/MainLayout/MainLayout";
 
 const Home = () => {
   return (
     <Animate>
       <Container>
-        <Navigation/>
-        <div className="py-12">
-         Hello Hello Hello Hello <br /> Hello Hello Hello
-          Hello Hello Hello <br /> Hello Hello Hello Hello Hello Hello <br />{" "}
-          Hello Hello Hello Hello Hello Hello <br /> Hello Hello Hello Hello
-          Hello Hello <br /> Hello Hello Hello Hello Hello Hello <br /> Hello
-          Hello Hello Hello Hello Hello <br /> Hello Hello Hello Hello Hello
-          Hello <br /> Hello Hello Hello Hello Hello Hello <br /> Hello Hello
-          Hello Hello Hello Hello <br /> Hello Hello Hello Hello Hello Hello{" "}
-          <br /> Hello Hello Hello Hello Hello Hello <br /> Hello Hello Hello
-          Hello Hello Hello <br /> Hello Hello Hello Hello Hello Hello <br />{" "}
-          Hello Hello Hello Hello Hello Hello <br />{" "}
+        <Navigation />
+        <div className="py-12 px-3">
+          <Search />
+          <MainLayout/>
         </div>
       </Container>
     </Animate>
